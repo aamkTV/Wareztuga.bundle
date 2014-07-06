6eeb197f97f0c0c9f1eaeb3e62fe2e17a808c28e
Jorge Galrito <jorge.galrito@me.com>
2014-07-06 10:14:06 +0100

    TV Shows support

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -24,14 +24,15 @@ def Start():
     VideoClipObject.thumb = R(ICON)
     VideoClipObject.art = R(ART)
 
+    Login()
+
 #####################################################################
 
 @handler('/video/wareztuga', TITLE)
 def MainMenu():
-    #Login()
     oc = ObjectContainer()
     oc.add(DirectoryObject(title='Filmes', key=Callback(MoviesMenu)))
-    #oc.add(DirectoryObject(title='Series', key=Callback(SeriesMenu)))
+    oc.add(DirectoryObject(title=unicode('Séries'), key=Callback(SeriesMenu)))
     #oc.add(DirectoryObject(title='Anime', key=Callback(AnimeMenu)))
     return oc
 
@@ -44,7 +45,7 @@ def MoviesMenu(page=1):
     movies_list = element.get_element_by_id('movies-list')
     
     for el in movies_list:
-        oc.add(ObjectForElement(el))
+        oc.add(MovieObjectForElement(el))
     
     oc.add(NextPageObject(key=Callback(MoviesMenu, page=int(page)+1), title='Next'))
     
@@ -53,44 +54,109 @@ def MoviesMenu(page=1):
 
 @route('/video/wareztuga/series', page=1)
 def SeriesMenu(page=1):
-    oc = ObjectContainer(title2='Series')
+    oc = ObjectContainer(title2=unicode('Séries'))
     
     element = HTML.ElementFromURL('http://www.wareztuga.tv/pagination.ajax.php?mediaType=series&p='+str(page))
     series_list = element.get_element_by_id('series-list')
     
     for el in series_list:
-        oc.add(ObjectForElement(el))
+        oc.add(SeriesObjectForElement(el))
     
     oc.add(NextPageObject(key=Callback(SeriesMenu, page=int(page)+1), title='Next'))
     
     return oc
 
 
-# Parse HTML to get media information
-def ObjectForElement(element):
-    #Movie information
-    movie_info_div = element.xpath('./div[2]')[0]
-    title = movie_info_div.xpath('./a')[0].text
-    url = BASE_URL + movie_info_div.xpath('./a/@href')[0]
-    year = int(movie_info_div.xpath('//span[@class="year"]/span')[0].tail)
-    summary = movie_info_div.get_element_by_id('movie-synopsis-aux').text
-    
-    # Thumbnail
-    img_tag = element.xpath('./div[1]/div//img')[0]
-    thumb_src = BASE_URL + img_tag.get('src')
 
-    return MovieObject(title=title, year=year, summary=summary, thumb=thumb_src, url=url)
+def SeasonsMenu(title, url):
+    html = HTML.ElementFromURL(url)
+    seasons_list = html.xpath('//div[@class="season"]')
+    oc = ObjectContainer(title2=title)
+    i = 0
+    for s in seasons_list:
+        i += 1
+        season_title = 'Temporada ' + str(s)
+        season_url = BASE_URL + s.xpath('./a/@href')[0]
+        season = SeasonObject(title=season_title + str(i),
+                              index=i,
+                              rating_key = season_url,
+                              key = Callback(EpisodesMenu,
+                                             series_title=title,
+                                             season_number=i,
+                                             url=season_url))
+        oc.add(season)
+    return oc
+
+
+def EpisodesMenu(series_title, season_number, url):
+    Log.Debug(series_title)
+    Log.Debug(url)
+    html = HTML.ElementFromURL(url)
+    episodes_list = html.xpath('//div[@id="episodes-list"]/div[@class!="item nextepisodes"]')
+    Log.Debug(episodes_list)
+    oc = ObjectContainer(title2=series_title)
+    for e in episodes_list:
+        episode = EpisodeObject(show = series_title,
+                                season = season_number,
+                                thumb = BASE_URL + e.xpath('.//img/@src')[0],
+                                title = e.xpath('.//img/@alt')[0],
+                                url = BASE_URL + e.xpath('.//a/@href')[0])
+        oc.add(episode)
+    return oc
+
+
+# Common parameters to Movies and TV Shows
+# Element is the div for the media
+def CommonParametersForElement(element):
+    title = element.xpath('./div[1]/div[1]/@title')[0]
+    url = BASE_URL + element.xpath('./div[1]/div[1]/a/@href')[0]
+    thumb_url = BASE_URL + element.xpath('./div[1]/div[1]/a/img/@src')[0]
+    summary = element.get_element_by_id('movie-synopsis-aux').text
+    return {'title': title,
+            'url': url,
+            'thumb_url': thumb_url,
+            'summary': summary}
+
+
+# Parse HTML to get media information
+def MovieObjectForElement(element):
+    common = CommonParametersForElement(element)
+    
+    movie_info_div = element.xpath('./div[2]')[0]
+    year = int(movie_info_div.xpath('//span[@class="year"]/span')[0].tail)
+    
+    return MovieObject(title=common['title'],
+                       year=year,
+                       summary=common['summary'],
+                       thumb=common['thumb_url'],
+                       url=common['url'])
+
+
+def SeriesObjectForElement(element):
+    common = CommonParametersForElement(element)
+    
+    nr_episodes = int(element.xpath('./div[1]/div[2]/span')[0].text)
+    genres = element.xpath('./div[2]/div[@class="movie-detailed-info"]/span[@class="genre"]')[0].text.split(', ')
+    
+    return TVShowObject(title=common['title'],
+                        summary=common['summary'],
+                        thumb=common['thumb_url'],
+                        key=Callback(SeasonsMenu, title=common['title'], url=common['url']),
+                        rating_key=common['url'],
+                        episode_count=nr_episodes,
+                        genres=genres)
 
 
 def Login():
-    url = BASE_URL + 'login.ajax.php?username=%s&password=%s' % (Prefs['username'], Prefs['password'])
-    request = HTTP.Request(url=url)
-    Log.Debug('Login response is: %s' % request.content)
+    username = Prefs['username']
+    password = Prefs['password']
 
+    Log.Debug('===> Attempting to login user "%s"' % username)
+    login_url = BASE_URL + 'login.ajax.php?username=%s&password=%s' % (username, password)
+    login_request = HTTP.Request(url=login_url, immediate=True)
+    login_response = login_request.content
+    Log.Debug('===> Login returned "%s"' % login_response)
 
-def Thumb(url):
-    try:
-        data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
-        return DataObject(data, 'image/jpeg')
-    except:
-        return Redirect(R(ICON))
+    if login_response == '0':
+        return True
+    return False
